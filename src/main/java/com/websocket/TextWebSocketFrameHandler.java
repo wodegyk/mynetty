632583414829905bbc37d27362628a47fc3e9229
admin <453148576@qq.com>
2019-04-30 00:35:46 +0800

    commit

---
+++ src/main/java/com/websocket/TextWebSocketFrameHandler.java
@@ -0,0 +1,39 @@
+package com.websocket;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
+
+import java.time.LocalDate;
+
+public class TextWebSocketFrameHandler extends SimpleChannelInboundHandler<TextWebSocketFrame> {
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, TextWebSocketFrame msg) throws Exception {
+        System.out.println("收到消息"+msg.text());
+        ctx.channel().writeAndFlush(new TextWebSocketFrame("服务器时间"+ LocalDate.now()));
+
+    }
+
+    @Override
+    public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("handlerAdded: "+ctx.channel().id().asLongText());
+    }
+
+
+    @Override
+    public void handlerRemoved(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("handlerRemoved: "+ctx.channel().id().asLongText());
+    }
+
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channelActive");
+    }
+
+
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channelInactive");
+    }
+}
